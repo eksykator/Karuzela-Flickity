738b4de4237cf40f17726f37a929888a6499a1a1
Marta Bobowska <martabobowska2@interia.pl>
2018-06-10 18:06:18 +0200

    Add marker to the map. Finish project

--- js/script.js
+++ js/script.js
@@ -31,35 +31,19 @@ flkty.on( 'scroll', function( progress ) {
 });
 
 function initMap() {
-    var bridge = {
-        lat: 51.1094937, 
-        lng: 17.0503735
-    };
+    var bridge = slidesData[0].coords;
     
     var map = new google.maps.Map(document.getElementById('map'),{
         zoom: 4,
         center: bridge
     });
     
-    var marker = new google.maps.Marker({
-        position: bridge,
-        map: map
-    });
+    var place;
+    for(var i = 0; i < slidesData.length; i++) {
+        place = slidesData[i].coords;
+        new google.maps.Marker({
+            position: place,
+            map: map
+        });
+    }
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
